4ffd910c28be96e0b47aac2d79daa5f01b3db06f
lvirrueta <luis510vg@gmail.com>
2024-09-13 19:16:28 -0600

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      filter: true,
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
+
+const swaggerOptions = new DocumentBuilder()
+  .setTitle('Basic Structure NestJs')
+  .setDescription('Basic Structure')
+  .setVersion('0.0.1')
+  .addBearerAuth()
+  .addSecurityRequirements('bearer')
+  .build();
