bd68185750a84bc5b5f0aae0953d029422ae7dc1
lvirrueta <luis510vg@gmail.com>
2024-09-13 22:51:37 -0600

    add jwt response in auth controller

--- src/auth/app/controller/auth.controller.ts
+++ src/auth/app/controller/auth.controller.ts
@@ -11,32 +11,36 @@ import { SignInDto } from '@auth/app/dto/sign-in.dto';
 
 // Api Model
 import { UserApi } from '@auth/app/swagger/user-api';
+import { AccessTokenApi } from '../swagger/access-token-api';
 
 // Models
 import { JsonResponse } from '@shared/app/model/json-response.class';
 
 // Decorator
+import { Public } from '../decorators/public.decorator';
 import { ApiJsonResponse } from '@shared/app/decorator/api-response.decorator';
 
 // Constants
 import { Routes } from '@shared/app/routes/routes.constants';
 
-@ApiExtraModels(UserApi)
+@ApiExtraModels(AccessTokenApi)
 @ApiTags(Routes.Auth.ApiTags)
 @Controller(Routes.Auth.Controller)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post(Routes.Auth.signUp)
-  @ApiJsonResponse({ status: HttpStatus.OK, type: UserApi })
+  @ApiJsonResponse({ status: HttpStatus.OK, type: AccessTokenApi })
   @ApiOperation({ summary: 'Register to the application', description: 'return an access token & refresh token' })
   async signUp(@Body() dto: SignUpDto): Promise<JsonResponse<UserApi>> {
     const data = await this.authService.signUp(dto);
     return new JsonResponse<UserApi>({ data });
   }
 
+  @Public()
   @Post(Routes.Auth.signIn)
-  @ApiJsonResponse({ status: HttpStatus.OK, type: UserApi })
+  @ApiJsonResponse({ status: HttpStatus.OK, type: AccessTokenApi })
   @ApiOperation({ summary: 'Login to the application', description: 'return an access token & refresh token' })
   async signIn(@Body() dto: SignInDto): Promise<JsonResponse<UserApi>> {
     const data = await this.authService.signIn(dto);
--- src/auth/domain/service/auth.service.ts
+++ src/auth/domain/service/auth.service.ts
@@ -1,12 +1,16 @@
 // Dependencies
+import * as ms from 'ms';
 import * as bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+import { JwtService } from '@nestjs/jwt';
 import { Inject, Injectable } from '@nestjs/common';
 
 // Repositories
 import { UserRepository } from 'src/auth/infrastructure/repositories/user.repository';
 
 // Interface
-import { IUser } from '../interface/i-user';
+import { IJwtPayload } from '../interface/i-jwt-payload';
+import { IAccessToken } from '../interface/i-access-token';
 import { IUserRepository } from '../irepositories/user.repository.interface';
 
 // Entity
@@ -16,32 +20,52 @@ import { UserEntity } from 'src/auth/infrastructure/entities/user.entity';
 import { SignUpDto } from 'src/auth/app/dto/sign-up.dto';
 import { SignInDto } from 'src/auth/app/dto/sign-in.dto';
 
+// Utilities
+import { ThrowError } from '@shared/app/utils/throw-error';
+
+// Types
+import { ID } from '@shared/app/types/types.types';
+
 // Constants
+import { Errors } from '@shared/app/error/error.constants';
 
 @Injectable()
 export class AuthService {
-  constructor(@Inject(UserRepository) public readonly userRepository: IUserRepository) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(UserRepository) public readonly userRepository: IUserRepository,
+  ) {}
 
   /** Login to the application */
-  public async signIn(dto: SignInDto): Promise<IUser> {
+  public async signIn(dto: SignInDto): Promise<IAccessToken> {
     const { password: passwordLogin, username } = dto;
     const userBD = await this.userRepository.findByUsername(username);
     const { password: passwordBD } = { ...userBD };
 
     if (this.comparePassword(passwordLogin, passwordBD) && userBD) {
-      return await userBD;
+      return await this.generateJwtToken(userBD.id);
     }
+
+    ThrowError.httpException(Errors.Auth.IncorrectCredentials);
   }
 
   /** Register to the application */
-  public async signUp(dto: SignUpDto): Promise<IUser> {
+  public async signUp(dto: SignUpDto): Promise<IAccessToken> {
     let { password, username } = dto;
 
     username = username.toLocaleLowerCase();
     password = await this.hashPassword(password);
 
     const user = new UserEntity({ username, password });
-    return await this.userRepository.saveEntity(user);
+    try {
+      await this.userRepository.saveEntity(user, { handleError: false });
+    } catch (e) {
+      if (e?.code === '23505') {
+        ThrowError.httpException(Errors.Auth.UserRegistered);
+      }
+      throw e;
+    }
+    return await this.generateJwtToken(user.id);
   }
 
   /** Hash Password */
@@ -53,5 +77,26 @@ export class AuthService {
   /** Compare Password login with password hashed in the DB */
   private comparePassword(passwordLogin: string, passwordUser: string): boolean {
     return bcrypt.compareSync(passwordLogin || '', passwordUser || '');
+  }
+
+  /** Generate JWT Token */
+  private async generateJwtToken(userID: ID): Promise<IAccessToken> {
+    const idToken = uuidv4();
+
+    const payload: IJwtPayload = {
+      userID,
+      jti: idToken,
+    };
+
+    const msTokenExpire = '1h';
+    const msRefreshTokenExpire = '2h';
+
+    const accessToken = this.jwtService.sign(payload, { expiresIn: ms(msTokenExpire) });
+    const refreshToken = this.jwtService.sign({ jti: idToken }, { expiresIn: ms(msRefreshTokenExpire) });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
