d01233fa7bc50500c00402a357916c264d21071c
lvirrueta <luis510vg@gmail.com>
2024-09-13 20:47:17 -0600

    add routes constants

---
+++ src/shared/app/routes/routes.constants.ts
@@ -0,0 +1,21 @@
+// const Search = 'search';
+// const List = 'get';
+// const Detail = 'get/:id';
+// const Create = 'create';
+// const Update = 'update';
+// const Delete = 'delete/:id';
+
+export const Routes = {
+  auth: {
+    ApiTags: `auth`,
+    Controller: `auth`,
+    // List,
+    // Search,
+    // Detail,
+    // Create,
+    // Update,
+    // Delete,
+    signUp: `sign-up`,
+    signIn: `sign-in`,
+  },
+};
