9d9135aa0c68c5bad211dc9417bdc301a81e0f9a
lvirrueta <luis510vg@gmail.com>
2024-09-13 20:29:12 -0600

    add findbyusername method

--- src/auth/infrastructure/repositories/user.repository.ts
+++ src/auth/infrastructure/repositories/user.repository.ts
@@ -3,20 +3,20 @@ import { DataSource } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 
 // Repository
+import { GenericRepository } from 'src/shared/infrastructure/repository/generic.repository';
 
 // Interface
+import { IUserRepository } from 'src/auth/domain/irepositories/user.repository.interface';
 
 // Entities
 import { UserEntity } from '../entities/user.entity';
-import { GenericRepository } from 'src/shared/infrastructure/repository/generic.repository';
 
 @Injectable()
-export class UserRepository extends GenericRepository<UserEntity> {
+export class UserRepository extends GenericRepository<UserEntity> implements IUserRepository {
   constructor(public readonly dataSource: DataSource) {
     super(UserEntity, dataSource);
   }
-
-  public async findByUsername(username: string): Promise<UserEntity> {
+  async findByUsername(username: string): Promise<UserEntity> {
     return await this.findOne({ where: { username } });
   }
 }
