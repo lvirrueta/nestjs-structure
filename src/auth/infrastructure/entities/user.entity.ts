e301b82387dfa457822b3f6bbd829ece21c833e5
lvirrueta <luis510vg@gmail.com>
2024-09-13 20:25:28 -0600

    add a constructor to user entity

--- src/auth/infrastructure/entities/user.entity.ts
+++ src/auth/infrastructure/entities/user.entity.ts
@@ -1,10 +1,17 @@
-import { UUID } from 'src/shared/app/types/types.types';
+import { IUser } from 'src/auth/domain/interface/i-user';
+import { ID } from 'src/shared/app/types/types.types';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'tblUsers' })
-export class UserEntity {
+export class UserEntity implements IUser {
+  constructor(dto?: IUser) {
+    const { password, username } = { ...dto };
+    this.password = password;
+    this.username = username;
+  }
+
   @PrimaryGeneratedColumn('uuid', { name: 'User_uuid' })
-  id: UUID;
+  id: ID;
 
   @Column({ name: 'User_strName', unique: true })
   username: string;
