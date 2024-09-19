1c139a8f5a5c08f4bd63e836d6bb2130e13f27c5
lvirrrueta <luis510vg@gmail.com>
2024-09-18 23:25:34 -0600

    create user service

--- src/auth/domain/service/user.service.ts
+++ src/auth/domain/service/user.service.ts
@@ -3,45 +3,70 @@ import * as bcrypt from 'bcrypt';
 import { Inject, Injectable } from '@nestjs/common';
 
 // Repositories
-import { UserRepository } from 'src/auth/infrastructure/repositories/user.repository';
+import {
+  UserRepository,
+  UserAdminRepository,
+  UserCustomerRepository,
+  UserOperativeRepository,
+} from '@auth/infrastructure/repositories/user.repository';
+
+// Entities
+import { UserEntity } from '@auth/infrastructure/entities/user/user.entity';
+import { UserAdminEntity } from '@auth/infrastructure/entities/user/user-admin.entity';
+import { UserCustomerEntity } from '@auth/infrastructure/entities/user/user-customer.entity';
+import { UserOperativeEntity } from '@auth/infrastructure/entities/user/user-operative.entity';
 
 // Dto
 import { UserDto } from '@auth/app/dto/user.dto';
 
 // Interface
-import { IUser } from '../interface/i-user';
-import { IUserRepository } from '../irepositories/user.repository.interface';
+import {
+  IUserRepository,
+  IUserAdminRepository,
+  IUserAbstractRepository,
+  IUserCustomerRepository,
+  IUserOperativeRepository,
+} from '@auth/domain/irepositories/user.repository.interface';
 
 // Types
 import { ID } from '@shared/app/types/types.types';
 
-@Injectable()
-export class UserService {
-  constructor(@Inject(UserRepository) public readonly userRepository: IUserRepository) {}
+export abstract class UserAbstractService<E extends UserEntity, R extends IUserAbstractRepository<E>> {
+  constructor(public readonly userRepository: R) {}
 
   /** Get All Users */
-  public async getUsers(): Promise<IUser[]> {
+  public async getUsers(): Promise<E[]> {
     return await this.userRepository.listEntities();
   }
 
   /** Get One User*/
-  public async detailUser(id: ID): Promise<IUser> {
+  public async detailUser(id: ID): Promise<E> {
     return await this.userRepository.findOneEntity(id);
   }
 
+  /** Create One User */
+  public async createUser(dto: UserDto): Promise<E> {
+    const { password } = dto;
+
+    dto.password = await this.hashPassword(password);
+    const e = this.userRepository.instanceEntity(dto);
+
+    return await this.userRepository.saveEntity(e);
+  }
+
   /** Update One User */
-  public async updateUser(dto: UserDto): Promise<IUser> {
+  public async updateUser(dto: UserDto): Promise<E> {
     const { id, password } = dto;
     const userBD = await this.userRepository.findOneEntity(id);
 
     if (!userBD) return;
-    dto.password = await this.hashPassword(password);
+    userBD.password = await this.hashPassword(password);
 
-    return await this.userRepository.updateEntity(dto);
+    return await this.userRepository.updateEntity(userBD);
   }
 
   /** Delete One User */
-  public async deleteUser(id: ID): Promise<IUser> {
+  public async deleteUser(id: ID): Promise<E> {
     return await this.userRepository.deleteEntity(id);
   }
 
@@ -49,5 +74,33 @@ export class UserService {
   private async hashPassword(password: string): Promise<string> {
     const salt = await bcrypt.genSalt();
     return await bcrypt.hash(password, salt);
+  }
+}
+
+@Injectable()
+export class UserService extends UserAbstractService<UserEntity, IUserRepository> {
+  constructor(@Inject(UserRepository) public readonly userRepository: IUserRepository) {
+    super(userRepository);
+  }
+}
+
+@Injectable()
+export class UserAdminService extends UserAbstractService<UserAdminEntity, IUserAdminRepository> {
+  constructor(@Inject(UserAdminRepository) public readonly userAdminRepository: IUserAdminRepository) {
+    super(userAdminRepository);
+  }
+}
+
+@Injectable()
+export class UserCustomerService extends UserAbstractService<UserCustomerEntity, IUserCustomerRepository> {
+  constructor(@Inject(UserCustomerRepository) public readonly userCustomerRepository: IUserCustomerRepository) {
+    super(userCustomerRepository);
+  }
+}
+
+@Injectable()
+export class UserOperativeService extends UserAbstractService<UserOperativeEntity, IUserOperativeRepository> {
+  constructor(@Inject(UserOperativeRepository) public readonly userOperativeRepository: IUserOperativeRepository) {
+    super(userOperativeRepository);
   }
 }
