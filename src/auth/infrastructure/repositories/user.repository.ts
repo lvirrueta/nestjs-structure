ad8ca4e6ac77e6ea930caf45da3468a9e50baa76
lvirrrueta <luis510vg@gmail.com>
2024-09-18 23:03:18 -0600

    create repositories for the operative, customer and admin entity

--- src/auth/infrastructure/repositories/user.repository.ts
+++ src/auth/infrastructure/repositories/user.repository.ts
@@ -6,17 +6,45 @@ import { Injectable } from '@nestjs/common';
 import { GenericRepository } from 'src/shared/infrastructure/repository/generic.repository';
 
 // Interface
-import { IUserRepository } from 'src/auth/domain/irepositories/user.repository.interface';
+import { IUserAbstractRepository } from '@auth/domain/irepositories/user.repository.interface';
 
 // Entities
-import { UserEntity } from '../entities/user.entity';
+import { UserEntity } from '../entities/user/user.entity';
+import { UserAdminEntity } from '@auth/infrastructure/entities/user/user-admin.entity';
+import { UserCustomerEntity } from '@auth/infrastructure/entities/user/user-customer.entity';
+import { UserOperativeEntity } from '@auth/infrastructure/entities/user/user-operative.entity';
+
+// @Injectable()
+export abstract class UserAbstractRepository<E extends UserEntity> extends GenericRepository<E> implements IUserAbstractRepository<E> {
+  public async findByUsername(username: string): Promise<E> {
+    return await this.findOne({ where: { username } as any });
+  }
+}
 
 @Injectable()
-export class UserRepository extends GenericRepository<UserEntity> implements IUserRepository {
+export class UserRepository extends UserAbstractRepository<UserEntity> {
   constructor(public readonly dataSource: DataSource) {
     super(UserEntity, dataSource);
   }
-  async findByUsername(username: string): Promise<UserEntity> {
-    return await this.findOne({ where: { username } });
+}
+
+@Injectable()
+export class UserAdminRepository extends UserAbstractRepository<UserAdminEntity> {
+  constructor(public readonly dataSource: DataSource) {
+    super(UserAdminEntity, dataSource);
+  }
+}
+
+@Injectable()
+export class UserOperativeRepository extends UserAbstractRepository<UserOperativeEntity> {
+  constructor(public readonly dataSource: DataSource) {
+    super(UserOperativeEntity, dataSource);
+  }
+}
+
+@Injectable()
+export class UserCustomerRepository extends UserAbstractRepository<UserCustomerEntity> {
+  constructor(public readonly dataSource: DataSource) {
+    super(UserCustomerEntity, dataSource);
   }
 }
