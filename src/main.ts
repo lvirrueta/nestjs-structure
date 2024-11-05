049db06df07debffd662cacacddc2a6a307d182f
lvirrueta <luis510vg@gmail.com>
2024-11-04 19:29:13 -0600

    add port loaded by env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
@@ -22,7 +22,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.verbose(`Running in port: ${port}`);
 }
 bootstrap();
 
